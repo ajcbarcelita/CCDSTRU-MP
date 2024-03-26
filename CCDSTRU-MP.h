10aaf1656ba8ca0095b1b2b11a7fec948e9b35aa
EdgarRykel <cody_casem@dlsu.edu.ph>
2024-03-26 12:45:53 +0800

    Progress_or_smth

--- CCDSTRU-MP.h
+++ CCDSTRU-MP.h
@@ -1,2 +1,45 @@
 #include <stdio.h>
 
+int
+Cardinality(int Board[][6])
+{
+    if(Board[0][0] == Board[0][2] && Board[0][2] == Board[1][1] && Board[1][1] == Board[2][0] && Board[2][0] == Board[2][2] && Board[2][2] == 1)
+    {
+        return 1;
+    }
+    if(Board[0][0] == Board[0][2] && Board[0][2] == Board[1][1] && Board[1][1] == Board[2][0] && Board[2][0] == Board[2][2] && Board[2][2] == 2)
+    {
+        return 1;
+    }
+}
+
+
+void
+NextPlayerMove( int PosR, int PosC, int * over, int * next, int * good, 
+                int Board[][6])
+{
+    int i, j, k;
+
+    int c = ((PosR-1)/3) + 1;
+    int d = ((PosC-1)/3) + 1;
+
+
+    if (!*over && !*next && Board[PosR-1][PosC-1] == 0 )
+    {
+        *good = !*good;
+        Board[PosR-1][PosC-1] = 1;
+    }
+
+    if (!*over && *next && Board[PosR-1][PosC-1] == 0 )
+    {
+        *good = !*good;
+        Board[PosR-1][PosC-1] = 2;
+    }
+
+    if (!*over && *good && *next && )
+
+    if (!*over && *good == 1) 
+    {
+        *good = !*good; 
+    }
+}
--- mpMain.c
+++ mpMain.c
@@ -3,6 +3,15 @@
  
  int main()
  {
- 	
+	int over = 0;
+	int next = 0;
+	int good = 0;
+	int PosR, PosC;
+
+	int Board[6][6] = {0}; // board is f 6x6 board
+	int Areas[2][2] = {0}; // areas is c 2x2 board -- win con
+
+
+ 	NextPlayerMove(PosR, PosC, &over, &next, &good, Board);
  	return 0;
  }
