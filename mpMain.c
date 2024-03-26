8031764336cd01cf30e08c5c118caa07ddc2d242
JohnathanTantanan <164973394+JohnathanTantanan@users.noreply.github.com>
2024-03-26 21:06:31 +0800

    Update mpMain.c

--- mpMain.c
+++ mpMain.c
@@ -5,13 +5,12 @@
  {
 	int over = 0;
 	int next = 0;
-	int good = 0;
 	int PosR, PosC;
 
 	int Board[6][6] = {0}; // board is f 6x6 board
 	int Areas[2][2] = {0}; // areas is c 2x2 board -- win con
 
 
- 	NextPlayerMove(PosR, PosC, &over, &next, &good, Board);
+ 	NextPlayerMove(PosR, PosC, &over, &next, Board);
  	return 0;
  }
